46d13f47f031aaaf38d097dd1a8bf7147b895300
gsmith660 <gsmith660@gmail.com>
2019-11-25 19:11:33 -0600

    steps 3 and 4 implemented

--- scripts.js
+++ scripts.js
@@ -6,11 +6,30 @@ window.addEventListener('load', function() {
     let shuttleBackgroundDiv = document.getElementById('shuttleBackground');
     let shuttleHeightParagraph = document.getElementById('spaceShuttleHeight');
 
+    let landButton = document.getElementById('landing');
+
+    let abortButton = document.getElementById('missionAbort');
+
     takeoffButton.addEventListener('click', function() {
         if (confirm('Confirm that the shuttle is ready for takeoff.')) {
             flightStatusParagraph.innerHTML = 'Shuttle in flight.';
             shuttleBackgroundDiv.style['background-color'] = 'blue';
             shuttleHeightParagraph.innerHTML = '10000';
+        }
+    });
+
+    landButton.addEventListener('click', function() {
+        alert('The shuttle is landing. Landing gear engaged.');
+        flightStatusParagraph.innerHTML = 'The shuttle has landed.';
+        shuttleBackgroundDiv.style['background-color'] = 'green';
+        shuttleHeightParagraph.innerHTML = '0';
+    });
+
+    abortButton.addEventListener('click', function() {
+        if (confirm('Confirm that you want to abort the mission.')) {
+            flightStatusParagraph.innerHTML = 'Mission aborted.';
+            shuttleBackgroundDiv.style['background-color'] = 'green';
+            shuttleHeightParagraph.innerHTML = '0';
         }
     });
 });
