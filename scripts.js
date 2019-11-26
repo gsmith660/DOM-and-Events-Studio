0bf2999d7d89bb5e3b6341f850a1755f6654bcf2
gsmith660 <gsmith660@gmail.com>
2019-11-25 21:27:54 -0600

    Bonus implemented

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function() {
     let shuttleImage = document.getElementById('rocket');
     shuttleImage.style.position = 'relative';
     shuttleImage.style.left = '0px';
-    shuttleImage.style.top = '0px';
+    shuttleImage.style.top = '122px';
 
     takeoffButton.addEventListener('click', function() {
         if (confirm('Confirm that the shuttle is ready for takeoff.')) {
@@ -33,6 +33,8 @@ window.addEventListener('load', function() {
         flightStatusParagraph.innerHTML = 'The shuttle has landed.';
         shuttleBackgroundDiv.style['background-color'] = 'green';
         shuttleHeightParagraph.innerHTML = '0';
+        shuttleImage.style.left = '0px';
+        shuttleImage.style.top = '122px';
     });
 
     abortButton.addEventListener('click', function() {
@@ -40,36 +42,61 @@ window.addEventListener('load', function() {
             flightStatusParagraph.innerHTML = 'Mission aborted.';
             shuttleBackgroundDiv.style['background-color'] = 'green';
             shuttleHeightParagraph.innerHTML = '0';
+            shuttleImage.style.left = '0px';
+            shuttleImage.style.top = '122px';
         }
     });
 
     rightButton.addEventListener('click', function() {
+        let backgroundWidth = shuttleBackgroundDiv.offsetWidth;
+        let shuttleWidth = shuttleImage.offsetWidth;
         let horizontalPosition = Number(shuttleImage.style.left.slice(0, -2));
-        horizontalPosition += 10;
-        shuttleImage.style.left = `${horizontalPosition}px`;
+        if (horizontalPosition < (backgroundWidth - shuttleWidth / 2) / 2 - 10) {
+            horizontalPosition += 10;
+            shuttleImage.style.left = `${horizontalPosition}px`;
+        } else {
+            alert('Shuttle Will Go Off Screen!');
+        }
     });
 
     leftButton.addEventListener('click', function() {
+        let backgroundWidth = shuttleBackgroundDiv.offsetWidth;
+        let shuttleWidth = shuttleImage.offsetWidth;
         let horizontalPosition = Number(shuttleImage.style.left.slice(0, -2));
-        horizontalPosition -= 10;
-        shuttleImage.style.left = `${horizontalPosition}px`;
+        if (horizontalPosition > -((backgroundWidth - shuttleWidth / 2) / 2 - 10)) {
+            horizontalPosition -= 10;
+            shuttleImage.style.left = `${horizontalPosition}px`;
+        } else {
+            alert('Shuttle Will Go Off Screen!');
+        }
+
     });
 
     upButton.addEventListener('click', function() {
         let verticalPosition = Number(shuttleImage.style.top.slice(0, -2));
-        verticalPosition -= 10;
-        shuttleImage.style.top = `${verticalPosition}px`;
-        shuttleHeight = Number(shuttleHeightParagraph.innerHTML);
-        shuttleHeight += 10000;
-        shuttleHeightParagraph.innerHTML = String(shuttleHeight);
+        if (verticalPosition > 3) {
+            verticalPosition -= 10;
+            shuttleImage.style.top = `${verticalPosition}px`;
+            shuttleHeight = Number(shuttleHeightParagraph.innerHTML);
+            shuttleHeight += 10000;
+            shuttleHeightParagraph.innerHTML = String(shuttleHeight);
+        } else {
+            alert('Shuttle Will Go Off Screen!');
+        }
     })
 
     downButton.addEventListener('click', function() {
+        let backgroundHeight = shuttleBackgroundDiv.offsetHeight;
+        let shuttleHeight = shuttleImage.offsetHeight;
         let verticalPosition = Number(shuttleImage.style.top.slice(0, -2));
-        verticalPosition += 10;
-        shuttleImage.style.top = `${verticalPosition}px`;
-        shuttleHeight = Number(shuttleHeightParagraph.innerHTML);
-        shuttleHeight -= 10000;
-        shuttleHeightParagraph.innerHTML = String(shuttleHeight);
+        if (verticalPosition < (backgroundHeight - shuttleHeight)){
+            verticalPosition += 10;
+            shuttleImage.style.top = `${verticalPosition}px`;
+            shuttleHeight = Number(shuttleHeightParagraph.innerHTML);
+            shuttleHeight -= 10000;
+            shuttleHeightParagraph.innerHTML = String(shuttleHeight);
+        } else {
+            alert('Shuttle Will Go Off Screen!');
+        }
     })
 });
